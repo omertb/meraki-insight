109e5d75fce99b27fd6eb1b6614d2e4e6a322005
omertb <omertugrul@gmail.com>
2020-11-13 16:45:06 +0300

    run port update

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ from project import app
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        app.run(host=sys.argv[1])
-    else:
-        app.run(host='0.0.0.0', port='5001')
+    app.run()
+#   if len(sys.argv) > 1:
+#       app.run(host=sys.argv[1])
+#   else:
+#       app.run(host='0.0.0.0', port='5001')
